a3ad7c5829b5fe33701ce33e230973cf2f5e227f
Matt McAlister <matt.mcalister93@gmail.com>
2017-11-08 17:17:18 +0000

    automatically backed up by learn

--- lib/advanced_jukebox.rb
+++ lib/advanced_jukebox.rb
@@ -44,7 +44,7 @@ def play(my_songs)
 
   if my_songs.keys.include?((response))
     puts "Playing #{response}"
-    system 'open #{my_songs[response]}'
+    system "open #{my_songs[response]}"
   else
     puts "Invalid input, please try again"
   end
