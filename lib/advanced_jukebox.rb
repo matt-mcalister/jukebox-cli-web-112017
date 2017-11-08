6e525a31c16c08087b9e196d41bef12f5266b051
Matt McAlister <matt.mcalister93@gmail.com>
2017-11-08 17:40:33 +0000

    automatically backed up by learn

--- lib/advanced_jukebox.rb
+++ lib/advanced_jukebox.rb
@@ -3,8 +3,8 @@
 #with the correct path to this directory on your computer
 
 my_songs = {
-"Go Go GO" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/01.mp3',
-"LiberTeens" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/02.mp3',
+"Go Go GO" => '/home/matt-mcalister/jukebox-cli-web-112017/audio/Emerald-Park/01.mp3',
+"LiberTeens" => '/home/matt-mcalister/jukebox-cli-web-112017/Emerald-Park/02.mp3',
 "Hamburg" =>  '< path to this directory >/jukebox-cli/audio/Emerald-Park/03.mp3',
 "Guiding Light" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/04.mp3',
 "Wolf" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/05.mp3',
